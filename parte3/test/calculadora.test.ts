dac16579dc61c678c6d9db95da4f9d981ebaa7e5
gfloriano11 <gflorianodev@gmail.com>
2026-04-06 19:55:52 -0300

    feat: skip first test

--- parte3/test/calculadora.test.ts
+++ parte3/test/calculadora.test.ts
@@ -9,7 +9,7 @@ import { Calculadora } from "../src/calculadora.js";
 
 describe("Função somar", () => {
     const calculadora = new Calculadora();
-    it.only("should return 3 when adding 1 and 2", () => {
+    it.skip("should return 3 when adding 1 and 2", () => {
         expect(calculadora.somar(1, 2)).toBe(3);
     })
     it("should return 5 when adding 2 and 3", () => {
