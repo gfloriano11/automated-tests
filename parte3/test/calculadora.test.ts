e60e1fae2477661964712a6f250b299f02cf477c
gfloriano11 <gflorianodev@gmail.com>
2026-04-06 19:55:11 -0300

    feat: add part 3 tests

---
+++ parte3/test/calculadora.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it, test } from "vitest";
+import { Calculadora } from "../src/calculadora.js";
+
+// test("Deve somar 10 + 10 e retornar 20", () => {
+//     const calculadora = new Calculadora();
+//     const resultado = calculadora.somar(10, 10);
+//     expect(resultado).toBe(20);
+// })
+
+describe("Função somar", () => {
+    const calculadora = new Calculadora();
+    it.only("should return 3 when adding 1 and 2", () => {
+        expect(calculadora.somar(1, 2)).toBe(3);
+    })
+    it("should return 5 when adding 2 and 3", () => {
+        expect(calculadora.somar(2, 3)).toBe(5);
+    })
+});
