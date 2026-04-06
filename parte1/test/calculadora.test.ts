ea3e21d0fab567f6d28d07a5618b3e90f9b63f5d
gfloriano11 <gflorianodev@gmail.com>
2026-04-06 19:35:30 -0300

    feat: add tests from first class

---
+++ parte1/test/calculadora.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { Calculadora } from "../src/calculadora";
+
+test("Deve somar 10 + 10 e retornar 20", () => {
+    const calculadora = new Calculadora();
+    const resultado = calculadora.somar(10, 10);
+    expect(resultado).toBe(20);
+})
